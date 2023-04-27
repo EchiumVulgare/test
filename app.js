df32b1e8ae5585544efa0066bd4de781b6c93268
EchiumVulgare <881507484pl@gmail.com>
2023-04-27 22:57:54 +0200

    test2

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const developer = {
-  name: "Alex",
-  surname: "Prybysh",
-  age: 30,
-  skills: ["JavaScript", "TypeScrip", "CSS"],
-  isMaried: false,
-  addAge() {
-    this.age++;
-    console.log(this);
-  },
-};
+// const developer = {
+//   name: "Alex",
+//   surname: "Prybysh",
+//   age: 30,
+//   skills: ["JavaScript", "TypeScrip", "CSS"],
+//   isMaried: false,
+//   addAge() {
+//     this.age++;
+//     console.log(this);
+//   },
+//   getMarried() {
+//     this.isMaried = true;
+//   },
+//   getDivorced() {
+//     this.isMaried = false;
+//   },
+// };
 
-developer.addAge();
+// developer.addAge();
+
+//////////////////////////////////////////////////////////////////
 
 // for (let key in developer) {
 //   console.log(key, developer[key]);
@@ -21,3 +29,9 @@ developer.addAge();
 // for (let number of numbers) {
 //   console.log(number);
 // }
+
+//////////////////////////////////////////////////////////////////
+
+//string methods
+const str = "Hello World";
+console.log(str.toLowerCase());
