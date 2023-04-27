fefd903a66346b525952e52d4c40610533817cf2
EchiumVulgare <881507484pl@gmail.com>
2023-04-28 00:19:24 +0200

    string methods test

--- app.js
+++ app.js
@@ -32,6 +32,23 @@
 
 //////////////////////////////////////////////////////////////////
 
-//string methods
-const str = "Hello World";
-console.log(str.toLowerCase());
+// //string methods
+// const str = "Hello World";
+// console.log(str.slice(6));
+// console.log(str.split(" ").join(" | "));
+// console.log(str.toUpperCase());
+// console.log(str.toLocaleLowerCase());
+
+//////////////////////////////////////////////////////////////////
+
+// const num = 11180000.15555;
+// console.log(num.toFixed(2));
+// console.log(num.toLocaleString());
+
+// const correctAnswer = "JavaScript";
+// const userAnswer = " javaScript! ";
+// const isCorrect = userAnswer
+//   .toLowerCase()
+//   .includes(correctAnswer.toLowerCase());
+
+// console.log(isCorrect);
