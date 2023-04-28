7f4681a9b5c73ac8e48582fc8f08232b9dfb4bed
EchiumVulgare <881507484pl@gmail.com>
2023-04-28 19:13:04 +0200

    test RegExp Errors Array methods

--- app.js
+++ app.js
@@ -31,8 +31,8 @@
 // }
 
 //////////////////////////////////////////////////////////////////
+//string methods !!!!!!!!!!!!!!!!
 
-// //string methods
 // const str = "Hello World";
 // console.log(str.slice(6));
 // console.log(str.split(" ").join(" | "));
@@ -52,3 +52,58 @@
 //   .includes(correctAnswer.toLowerCase());
 
 // console.log(isCorrect);
+
+//////////////////////////////////////////////////////////////////
+// ARRAY METHODS !!!!!!!!!!!!!!!!
+
+// const numbers = [1, 2, 3, 4];
+
+// //POP, PUSH
+// // SHIFT, UNSHIFT
+
+// console.log(numbers.includes(4)); //True
+// console.log(numbers.indexOf(2)); // 1
+// console.log(numbers.indexOf(9)); // -1
+
+// console.log(numbers.slice(0, 2)); // new Arr [1, 2];
+
+// const nums2 = numbers.concat([5, 6, 7]); // concotenation two Arrays numbers + new Arr nums2
+// console.log(nums2);
+
+// console.log(numbers.join(", ")); // получим строку с ,  u пробелом;
+
+//////////////////////////////////////////////////////////////////
+
+// ERRORS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// function sum(a, b) {
+//   if (typeof a !== "number" || typeof b !== "number") {
+//     throw new Error("В фукцию не переданы числa для сложения");
+//   }
+//   return a + b;
+// }
+
+// try {
+//   sum();
+// } catch (err) {
+//   console.error(err);
+// }
+
+// console.log("end");
+
+//////////////////////////////////////////////////////////////////
+
+// RegExp !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// const regex1 = /\w+/;
+// const regex2 = new RegExp("\\w+");
+
+// const str = "JavaScript is cool laguage. Everybody should learn JavaScript";
+// console.log(/JavaScript/i.test(str)); // i luboj register A or a;
+
+// console.log(str.replace(/javascript/gi, "php")); //g - global
+
+// console.log(str.match(/JavaScript/gi));
+
+//////////////////////////////////////////////////////////////////
+//
+
