1ed7f863858412cfeaa3ffaaa56b683e8af6fc12
EchiumVulgare <881507484pl@gmail.com>
2023-05-03 00:15:23 +0200

    test function

--- app.js
+++ app.js
@@ -435,6 +435,7 @@
 //   return (num) => x + num;
 // }
 
+
 // const addByTwo = addByX(2);
 // addByTwo(3); //5
 // addByTwo(5); //7
@@ -519,3 +520,57 @@
 // supporter2.chant = supporter1.chant.bind(supporter2);
 // supporter2.chant();
 
+// let age = prompt('How old are you?', 18);
+
+// function checkAge(age) {
+//   if (age >=18){
+//     return true;
+//   } else {
+//     return confirm('A mama razreszala?')
+//   }
+// }
+
+// if( checkAge(age) ) {
+//   alert( 'Access accept' );
+// } else {
+//   alert( 'Access denied' );
+// }
+
+// function checkAge(age){
+//   if(age >= 18){
+//     return true;
+//   }
+//   return confirm('Roditeli razreszili ?')
+// }
+
+// function checkAge(age){
+//   return (age >= 18) ? confirm('A ty uże bolszoj') : confirm("roditeli razreszali"); //WARIANT ?-togda : - inache
+//   return (age >= 18) || confirm('Roditeli razreszalai'); //wariant s ||
+// }
+
+// function min(a,b) {
+//   if(a < b){
+//     return a
+//   } else {
+//     return b
+//   }
+// }
+
+// function min(a,b) {
+//   return a < b ? a : b;
+// }
+
+//////////////////////////////////////////
+// Напишите функцию pow(x,n), которая возводит x в степень n и возвращает результат.
+
+// pow(3, 2) = 3 * 3 = 9
+// pow(3, 3) = 3 * 3 * 3 = 27
+// pow(1, 100) = 1 * 1 * ...* 1 = 1
+// Создайте страницу, которая запрашивает x и n, а затем выводит результат pow(x,n).
+
+// function pow(x, n) {
+//   return x ** n;
+// }
+// let x = prompt('BBedite chislo x', '');
+// let n = prompt('BBedite stepen', '');
+// n < 1 ? alert(`Stepen ${n} nie podderziwaetsia ispolzujte naturalnoje chislo`) : alert(pow(x,n));
