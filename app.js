7fd04e2b3a6c44684b85578e1b3cc1b470a0b32f
EchiumVulgare <881507484pl@gmail.com>
2023-05-04 00:04:22 +0200

    test map and filter

--- app.js
+++ app.js
@@ -801,3 +801,157 @@
 // }));
 
 // console.log(middleDevelopers);
+
+////////////////////////////////FILTER///////////////////////////////////////////////////
+////////////////////////////////FILTER///////////////////////////////////////////////////
+////////////////////////////////FILTER///////////////////////////////////////////////////
+////////////////////////////////FILTER///////////////////////////////////////////////////
+////////////////////////////////FILTER///////////////////////////////////////////////////
+// фильтрация так же должна быть иммутабельна. Мне не должны ломать оригинальый массив. Мы должны создавать новую структуру данных, сохраняя туда отфильтрованные значения.
+// примитивные массивы в которых лежат строки, числа, undefined, booleen/ [1,'hi', undefined, true, false]; Массив с примитивами называют массивами, а массивы в которых лежат обЪект называют - Коллекциями.
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// const users = [
+//   {
+//     id: 1,
+//     name: "Brad Pette",
+//     movies: ["Snatch", "Inglorious Basterdes"],
+//     isMarried: true,
+//   },
+//   {
+//     id: 2,
+//     name: "Diana Moore",
+//     movies: ["Inglorious Basterdes"],
+//     isMarried: false,
+//   },
+//   {
+//     id: 3,
+//     name: "Olivie Giroud",
+//     movies: ["Inglorious Basterdes"],
+//     isMarried: true,
+//   },
+//   {
+//     id: 4,
+//     name: "Scott Parker",
+//     movies: ["Inglorious Basterdes"],
+//     isMarried: false,
+//   },
+// ];
+
+// // const oddNumbers = numbers.filter((num) => num % 2 !== 0); // expected odd numbers [1, 3, 5, 7];
+// // const evenNumbers = numbers.filter((num) => num % 2 === 0); // expected even numbers [2, 4, 6, 8];
+
+// // console.log(oddNumbers);
+// // console.log(evenNumbers);
+
+// const marriedUsers = users.filter((user) => user.isMarried === true); //true
+// const notMarriedUsers = users.filter((user) => !user.isMarried); //false
+// const snatchUsers = users.filter((user) => user.movies.includes("Snatch")); //includes
+// console.log(snatchUsers);
+// console.log(marriedUsers);
+// console.log(notMarriedUsers);
+
+////////////////////////////////PRACTICE///////////////////////////////////////////////////
+////////////////////////////////PRACTICE///////////////////////////////////////////////////
+////////////////////////////////PRACTICE///////////////////////////////////////////////////
+////////////////////////////////PRACTICE///////////////////////////////////////////////////
+////////////////////////////////PRACTICE///////////////////////////////////////////////////
+
+// Практика на фильтрацию
+// 1) Дан массив строк names. Создайте новый массив shortNames, сохранив в него только значения из оригинального массива, длина которых не превышает 5 символов.
+
+// 2) Дана коллекция products. Создайте на базе нее новую коллекцию cheapProducts, поместив в нее только элементы оригинальной коллекции, ключ price в которых ниже 300.
+
+// 3) Дана коллекция developers, где у каждого элемента имеется свойство skills. Создайте новую коллекцию tsDevelopers, добавив туда только те элементы оригинальной коллекции, у которой в массиве skills есть значение 'TypeScript'.
+
+// const names = [
+//   "Mikhail",
+//   "Ivan",
+//   "Albert",
+//   "John",
+//   "Ian",
+//   "Petr",
+//   "Alexandr",
+//   "Oleg",
+//   "Jaroslav",
+//   "Vsevolod",
+// ];
+
+// const shortNames = names.filter((el) => el.length <= 5);
+// console.log(shortNames);
+
+////////////////////////////////PRACTICE 2///////////////////////////////////////////////////
+
+// const products = [
+//   {
+//     id: 1,
+//     name: "Intro to JavaScript",
+//     category: "books",
+//     price: 150,
+//   },
+//   {
+//     id: 2,
+//     name: "Intro to CSS",
+//     category: "books",
+//     price: 120,
+//   },
+//   {
+//     id: 3,
+//     name: "Deep dive into JavaScript",
+//     category: "books",
+//     price: 350,
+//   },
+//   {
+//     id: 4,
+//     name: "Intro to PHP",
+//     category: "books",
+//     price: 250,
+//   },
+//   {
+//     id: 5,
+//     name: "Deep dive into PHP",
+//     category: "books",
+//     price: 350,
+//   },
+// ];
+
+// const cheapProducts = products.filter((product) => product.price <= 300);
+// console.log(cheapProducts);
+
+////////////////////////////////PRACTICE 3///////////////////////////////////////////////////
+
+// const developers = [
+//   {
+//     id: 1,
+//     fullName: "Ivan Ivanov",
+//     skills: ["HTML", "CSS", "Git", "Gulp", "Pug"],
+//   },
+//   {
+//     id: 2,
+//     fullName: "Petr Petrov",
+//     skills: ["HTML", "CSS", "Git", "JavaScript", "npm"],
+//   },
+//   {
+//     id: 3,
+//     fullName: "Ian Melnikov",
+//     skills: ["HTML", "CSS", "Git", "JavaScript", "TypeScript"],
+//   },
+//   {
+//     id: 4,
+//     fullName: "Antonio Banderas",
+//     skills: ["HTML", "CSS", "Git", "JavaScript", "TypeScript", "React"],
+//   },
+// ];
+
+// // const snatchUsers = users.filter((user) => user.movies.includes("Snatch")); //includes
+// const tsDevelopers = developers.filter((el) =>
+//   el.skills.includes("TypeScript")
+// );
+// console.log(tsDevelopers);
+
+
+////////////////////////////////FILTER///////////////////////////////////////////////////
+////////////////////////////////FILTER///////////////////////////////////////////////////
+////////////////////////////////FILTER///////////////////////////////////////////////////
+////////////////////////////////FILTER///////////////////////////////////////////////////
+////////////////////////////////FILTER///////////////////////////////////////////////////
