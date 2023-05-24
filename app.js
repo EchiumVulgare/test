0055166b8feb0701a577d21a066262cbbab0694f
Aleks <example@gmail.com>
2023-05-24 23:41:49 +0200

    metka first test

--- app.js
+++ app.js
@@ -1613,15 +1613,24 @@
 
 'use strict';
 
-//test commit start work
-// tetet
-// tetet
-// tetet
-// tetet
-// tetet
-// tetet
-// tetet
-// tetet
-// tetet
-// tetet
-// tetet
+// let result = '';
+
+// for (let i = 0; i <= 7; i++) {
+//   for (let j = 1; j < i; j++) {
+//     result += '*';
+//   }
+//   result += '\n';
+// }
+
+// console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+  console.log(`First level: ${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`Second level: ${j}`);
+    for (let k = 0; k < 3; k++) {
+      if (k === 2) continue first;
+      console.log(`Third level: ${k}`);
+    }
+  }
+}
