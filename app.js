09114f9df1c55fa4e9d745b8b5ce98d899ade1d6
Alex <881507484pl@gmail.com>
2023-05-26 00:03:15 +0200

    add scrip.js

--- app.js
+++ app.js
@@ -1793,16 +1793,16 @@
 // ***********
 // (Подсказка: в конце фигуры есть перенос строки \n, который тоже учитывается в тестах. В КОНЦЕ КАЖДОЙ СТРОКИ НЕТ ПРОБЕЛОВ, ТОЛЬКО ПЕРЕНОС)
 
-const lines = 5;
-let result = '';
+// const lines = 5;
+// let result = '';
 
-for (let i = 0; i <= lines; i++) {
-  for (let j = 0; j < lines - i; j++) {
-    result += ' ';
-  }
-  for (let j = 0; j < 2 * i + 1; j++) {
-    result += '*';
-  }
-  result += '\n';
-}
-console.log(result);
+// for (let i = 0; i <= lines; i++) {
+//   for (let j = 0; j < lines - i; j++) {
+//     result += ' ';
+//   }
+//   for (let j = 0; j < 2 * i + 1; j++) {
+//     result += '*';
+//   }
+//   result += '\n';
+// }
+// console.log(result);
---
+++ script.js
@@ -0,0 +1,26 @@
+const numberOfFilms = +prompt('How much films do you already watch', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt('One of the last film wich you have watched is ?', '');
+  const b = prompt('Please rate this film', '');
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if(personalMovieDB.count)
+
+console.log(personalMovieDB);
