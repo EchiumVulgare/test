f61896b2a4c1f980031e5bd09fbc3adc063de92a
Aleks <881507484pl@gmail.com>
2023-06-04 22:12:27 +0200

    fibonachi f exercise

--- app.js
+++ app.js
@@ -2268,18 +2268,65 @@
 
 // findMaxNumber(1, 5, '6', '10');  =>  0
 
-function findMaxNumber(a, b, c, d) {
-  if (
-    typeof a !== 'number' ||
-    typeof b !== 'number' ||
-    typeof c !== 'number' ||
-    typeof d !== 'number'
-  ) {
-    return 0;
-  } else {
-    return Math.max(a, b, c, d);
+// function findMaxNumber(a, b, c, d) {
+//   if (
+//     typeof a !== 'number' ||
+//     typeof b !== 'number' ||
+//     typeof c !== 'number' ||
+//     typeof d !== 'number'
+//   ) {
+//     return 0;
+//   } else {
+//     return Math.max(a, b, c, d);
+//   }
+// }
+
+// console.log(findMaxNumber(1, 5, '66', '10'));
+// console.log(findMaxNumber(1, 5, 3.9, 8.5));
+
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+// Создайте функцию, которая будет принимать в себя один аргумент-целое положительное число. Она должна возвращать строку, в которой будут через пробел выведены числа Фибоначчи. Причем, их количество должно быть равно переданному аргументу.
+
+// Если переданный аргумент не число - вернуть пустую строку. Решать без применения рекурсии.
+
+// Пример:
+
+// fib(4) => ''0 1 1 2"
+
+// fib(7) => ''0 1 1 2 3 5 8"
+
+// fib('7') => ''"
+
+// fib(1) => "0"
+
+// fib(0) => ''"
+
+function fib(num) {
+  if (typeof num !== 'number' || num <= 0 || !Number.isInteger(num)) {
+    return '';
   }
+
+  let result = '';
+  let first = 0;
+  let second = 1;
+
+  for (let i = 0; i < num; i++) {
+    if (i + 1 === num) {
+      result += `${first}`;
+      // Без пробела в конце
+    } else {
+      result += `${first} `;
+    }
+
+    let third = first + second;
+    first = second;
+    second = third;
+  }
+
+  return result;
 }
 
-console.log(findMaxNumber(1, 5, '66', '10'));
-console.log(findMaxNumber(1, 5, 3.9, 8.5));
+console.log(fib(7));
