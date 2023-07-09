25aa622e7d321aaa41b191a73a7700236ae0ebf4
Aleks <881507484pl@gmail.com>
2023-07-09 16:33:18 +0200

    09.07.2023

--- app.js
+++ app.js
@@ -2337,6 +2337,31 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 
+// function first() {
+//   //Do something
+//   setTimeout(function () {
+//     console.log(1);
+//   }, 500);
+// }
+
+// function second() {
+//   console.log(2);
+// }
+
+// first();
+// second();
+
+function learnJS(lang, callback) {
+  console.log(`Iam lerning: ${lang}`);
+  callback();
+}
+
+function done() {
+  console.log('Iam finishing this leason!');
+}
+
+learnJS('JavaScript', done);
+
 // function learnJS(lang, callback) {
 //   console.log(`Я учу: ${lang}`);
 //   callback();
